2000c37b8bb404a00ef9b191e4ddd54280ffbb0b
Hariprasath <balaprasad24@gmail.com>
2020-04-24 00:50:18 +0530

    Committed the solutions

---
+++ april-interview-problems/LogTracing.java
@@ -0,0 +1,87 @@
+package in.hp.java8.exam;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+public class LogTracing {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("0 A created",
+                "0 A created",
+                "1 B created",
+                "2 C created",
+                "3 D created",
+                "10 A running",
+                "11 A waiting",
+                "12 B waiting",
+                "13 B running",
+                "14 C running",
+                "17 B terminated",
+                "18 A terminated");
+        System.out.println(traceLog(14, list));
+    }
+
+    static String traceLog(int timing, List<String> logs) {
+        final String CREATED = "created";
+        final String WAITING = "waiting";
+        final String RUNNING = "running";
+        final String TERMINATED = "terminated";
+
+        class Log {
+            String process;
+            String state;
+            int time;
+
+            public Log(String process, String state, int time) {
+                this.process = process;
+                this.state = state;
+                this.time = time;
+            }
+
+            @Override
+            public String toString() {
+                return "Log{" +
+                        "process='" + process + '\'' +
+                        ", state='" + state + '\'' +
+                        ", time=" + time +
+                        '}';
+            }
+        }
+
+        int timeBefore = -1;
+
+        LinkedHashMap<String, Log> processes = new LinkedHashMap<>();
+
+        for (int i = 0; i < logs.size(); i++) {
+            String[] s = logs.get(i).split("\\s");
+            if (s[2].equals(TERMINATED))
+                break;
+            if (null != processes.get(s[1])) {
+                Log log = processes.get(s[1]);
+                log.time = Integer.valueOf(s[0]);
+                log.state = s[2];
+                processes.put(s[1], log);
+            } else {
+                processes.put(s[1], new Log(s[1], s[2], Integer.valueOf(s[0])));
+            }
+        }
+
+        String lastRunningProcess = "-1";
+        Object[] processNames = processes.keySet().toArray();
+        for (int i = 0; i < processNames.length; i++) {
+            Log log = processes.get(processNames[i]);
+            if (log.state.equals(RUNNING) && log.time <= timing) {
+                if (lastRunningProcess.equals("-1"))
+                    lastRunningProcess = log.process;
+                else {
+                    lastRunningProcess = "-1";
+                    break;
+                }
+            }
+        }
+
+        System.out.println(processes);
+        return lastRunningProcess;
+    }
+
+}
---
+++ april-interview-problems/MinimumNumberOfProblem.java
@@ -0,0 +1,43 @@
+package in.hp.java8.exam;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MinimumNumberOfProblem {
+    public static void main(String[] args) {
+        List<Integer> points = Arrays.asList(1,2,3,4,5);
+        int threshold = 4;
+        System.out.println("Minimum Number of Problems to solve: "
+                + MinimumNumberOfProblem.minNum(threshold, points));
+    }
+
+    public static int minNum(int threshold, List<Integer> points) {
+        int minimumPoint = points.get(0);
+        int maxPointIndex = -1;
+        int result = 0;
+
+        if (threshold == minimumPoint)
+            return 1;
+
+        int j = 0;
+        while (j < points.size() && maxPointIndex < 0) {
+            if (points.get(j) - minimumPoint >= threshold) {
+                maxPointIndex = j;
+            }
+            j++;
+        }
+
+        if (maxPointIndex < 0) {
+            return points.size();
+        }
+
+        for (int i = 0; i <= maxPointIndex; i++) {
+            if (i + 2 <= maxPointIndex) {
+                i++;
+            }
+            result++;
+        }
+
+        return result;
+    }
+}
